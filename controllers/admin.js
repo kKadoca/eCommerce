22e814a72c0d3329b2ddad69694e45e8b829b0e3
Kadu Terra <kaduterra74@hotmail.com>
2023-02-20 10:40:36 -0300

    cart complete

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,14 +13,15 @@ exports.postAddProductPage = (req, res, next) => {
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
-    Product.create({
+    req.user.createProduct({
         title: title,
         price: price,
         imageUrl: imageUrl,
-        description: description
+        description: description,
+        userId: req.user.id
     })
-        .then(result => res.redirect('/admin/products'))
-        .catch(err => console.log(err));
+    .then(result => res.redirect('/admin/products'))
+    .catch(err => console.log(err));
 }
 
 exports.getEditProductPage = (req, res, next) => {
@@ -30,8 +31,12 @@ exports.getEditProductPage = (req, res, next) => {
     }
 
     const prodId = req.params.productId;
-    Product.findByPk(prodId)
-    .then(product => {
+    req.user.getProducts({where: {id: prodId}})
+    .then(products => {
+        const product = products[0];
+        if (!product) {
+            return res.redirect('/');
+        }
         res.render('admin/edit-product', {
             docTitle: 'Edit Product',
             path: '/admin/edit-product',
--- controllers/shop.js
+++ controllers/shop.js
@@ -9,7 +9,6 @@ exports.getProductsPage = (req, res, next) => {
             docTitle: 'All Products',
             path: '/products',
         });
-
     })
     .catch(err => console.log(err));
 }
@@ -41,38 +40,67 @@ exports.getIndexPage = (req, res, next) => {
 }
 
 exports.getCartPage = (req, res, next) => {
-    Cart.getCart(cart => {
-        Product.fetchAll(products => {
-            const cartProducts = [];
-            for (product of products) {
-                const cartProductData = cart.products.find(prod => prod.id === product.id);
-                if (cartProductData) {
-                    cartProducts.push({productData: product, qty: cartProductData.qty});
-                }
-            }
+    req.user.getCart()
+    .then(cart => {
+        return cart.getProducts()
+        .then(products => {
             res.render('shop/cart', {
                 path: '/cart',
                 docTitle: 'Your Cart', 
-                products: cartProducts
+                products: products
             });
-        });
-    });
+        })
+        .catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
 }
 
 exports.postCartPage = (req, res, next) => {
-    const productId = req.body.productId;
-    Product.findById(productId, product => {
-        Cart.addProduct(productId, product.price);
+    const prodId = req.body.productId;
+    let fetchedCart;
+    let newQuantity = 1;
+    req.user.getCart()
+    .then(cart => {
+        fetchedCart = cart;
+        return cart.getProducts({ where: {id: prodId}})
     })
-    res.redirect('/cart');
+    .then(products => {
+        let product;
+        if (products.length > 0) {
+            product = products[0];
+        }
+        if (product) {
+            const oldQuantity = product.cartItem.quantity;
+            newQuantity = oldQuantity + 1;
+            return product;
+        }
+        return Product.findByPk(prodId)
+    })
+    .then(product => {
+        return fetchedCart.addProduct(product, {
+            through: { quantity: newQuantity } 
+       });
+    })
+    .then(() => {
+        res.redirect('/cart');
+    })
+    .catch(err => console.log(err));
 }
 
 exports.postCartDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
-    Product.findById(prodId, product => {
-        Cart.deleteProduct(prodId, product.price);
+    req.user.getCart()
+    .then(cart => {
+        return cart.getProducts({ where: { id: prodId }})
+    })
+    .then(products => {
+        const product = products[0];
+        return product.cartItem.destroy();
+    })
+    .then(result => {
         res.redirect('/cart');
-    });
+    })
+    .catch(err => console.log(err));
 }
 
 exports.getOrdersPage = (req, res, next) => {
